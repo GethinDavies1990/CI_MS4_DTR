6b20fcb81ea4ee5756b21ce759cafb7d08cee062
GethinDavies1990 <gethindavies1990@gmail.com>
2023-06-17 09:42:57 +0000

    add stripe js intent, test the result is successful

--- checkout/views.py
+++ checkout/views.py
@@ -30,13 +30,13 @@ def checkout(request):
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set your environment variable?')
+            Did you forget to set it in your environment?')
 
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'stripe_public_key',
-        'client_secret': 'intent.stripe_secret_key',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
