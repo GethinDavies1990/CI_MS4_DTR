1f32830b3fc1b0d601f296395ce1d01203d637fa
GethinDavies1990 <gethindavies1990@gmail.com>
2023-06-22 22:33:50 +0000

    add temp superuser

--- manage.py
+++ manage.py
@@ -20,3 +20,14 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+if not User.objects.filter(is_superuser=True).first():
+    user = User.objects.create(
+        username = 'admin',
+        email = 'admin@mywebsite.com',
+        is_superuser = True,
+        ...
+    )
+    user.set_password('some password')
+    user.save()
--- taco_y_tequila/settings.py
+++ taco_y_tequila/settings.py
@@ -221,13 +221,3 @@ DEFAULT_FROM_EMAIL = 'gethindavies1990@gmail.com'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-if not User.objects.filter(is_superuser=True).first():
-    user = User.objects.create(
-        username = 'admin',
-        email = 'admin@mywebsite.com',
-        is_superuser = True,
-        ...
-    )
-    user.set_password('some password')
-    user.save()
