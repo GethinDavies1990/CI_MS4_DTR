7d3ff87b48e384e65b2c7a5d7e4868efc246736f
GethinDavies1990 <gethindavies1990@gmail.com>
2023-06-22 22:26:45 +0000

    superuser

--- taco_y_tequila/settings.py
+++ taco_y_tequila/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*', 'taco-y-tequila-6c36dccf242c.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [ '*', 'taco-y-tequila-6c36dccf242c.herokuapp.com', 'localhost', '8000-gethindavies19-cims4dtr-g8g3rbb7xpp.ws-eu100.gitpod.io']
 
 
 CSRF_TRUSTED_ORIGINS = [
@@ -221,3 +221,13 @@ DEFAULT_FROM_EMAIL = 'gethindavies1990@gmail.com'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not User.objects.filter(is_superuser=True).first():
+    user = User.objects.create(
+        username = 'admin',
+        email = 'admin@mywebsite.com',
+        is_superuser = True,
+        ...
+    )
+    user.set_password('some password')
+    user.save()
