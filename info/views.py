69814f92848e717336a08825d3b5b8900d932b47
GethinDavies1990 <gethindavies1990@gmail.com>
2023-06-25 19:30:19 +0000

    fix: issue where data wasnt outputting to the html page

--- info/views.py
+++ info/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render
 from .models import Teams
 
 def meet_the_team(request):
     """
     A view to show all staff members
     """
-    teams = Teams()
+    teams = Teams.objects.all()  # Fetch all Teams objects from the database
 
     context = {
         'teams': teams,
     }
 
     return render(request, 'meet_the_team.html', context)
+
