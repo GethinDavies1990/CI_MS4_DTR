1c1ab7da1b344da3be01422d49b6bbc8e59e3de3
GethinDavies1990 <gethindavies1990@gmail.com>
2023-06-28 19:43:32 +0000

    add add_prodict view

--- products/views.py
+++ products/views.py
@@ -3,8 +3,8 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ProductForm
 
-# Create your views here.
 
 def all_products(request):
     """
@@ -71,3 +71,15 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add product to store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
