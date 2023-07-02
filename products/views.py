a65d0b468da72c7448b74dc3d76601699713b973
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-02 12:01:37 +0000

    set debug to true

--- products/views.py
+++ products/views.py
@@ -9,53 +9,6 @@ from .forms import ProductForm
 
 
 def all_products(request):
-    """ A view to show all products, including sorting and search queries """
-
-    products = Product.objects.all()
-    query = None
-    categories = None
-    sort = None
-    direction = None
-
-    if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
-            
-        if 'category' in request.GET:
-            categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
-
-        if 'q' in request.GET:
-            query = request.GET['q']
-            if not query:
-                messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
-
-    current_sorting = f'{sort}_{direction}'
-
-    context = {
-        'products': products,
-        'search_term': query,
-        'current_categories': categories,
-        'current_sorting': current_sorting,
-    }
-
-    return render(request, 'products/products.html', context)
     """
     A view to show all products, including sorting and searching
     """
@@ -73,12 +26,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-            elif sortkey == 'category':
+            if sortkey == 'category':
                 sortkey = 'category__name'
-            elif sortkey == 'spice_asc':
-                sortkey = 'spice'
-            elif sortkey == 'spice_desc':
-                sortkey = '-spice'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
@@ -192,4 +141,3 @@ def delete_product(request, product_id):
     product.delete()
     messages.success(request, 'Product Deleted!')
     return redirect(reverse('products'))
-
