85535650cb3a608357c260021be941568185fd18
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-05 19:10:09 +0000

    refactor: checkout/urls.py

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,11 +1,22 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.urls import path
+
+# Internal:
 from . import views
 from .webhooks import webhook
 
-urlpatterns = [
-    path('', views.checkout, name='checkout'),
-    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
-    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    path('wh/', webhook, name='webhook'),
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+urlpatterns = [
+    path("", views.checkout, name="checkout"),
+    path(
+        "checkout_success/<order_number>",
+        views.checkout_success,
+        name="checkout_success",
+    ),
+    path("cache_checkout_data/", views.cache_checkout_data, name="cache_checkout_data"),
+    path("wh/", webhook, name="webhook"),
 ]
