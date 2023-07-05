89ecea66ed85dc020d245e8ed96dc722058fff69
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-05 19:08:49 +0000

    refactor: checkout/signals.py

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,12 +1,26 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
+# Internal:
 from .models import OrderLineItem
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
+    Args:
+        sender (object): Sender object
+        instance: Instance object
+        created: created
+        **kwargs: **kwargs
+    Returns:
+        N/A
     """
     instance.order.update_total()
 
@@ -15,5 +29,11 @@ def update_on_save(sender, instance, created, **kwargs):
 def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
+    Args:
+        sender (object): Sender object
+        instance: Instance object
+        **kwargs: **kwargs
+    Returns:
+        N/A
     """
     instance.order.update_total()
