d859c2206b2bde47968cabe77a33406f5fc2f895
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-05 19:54:13 +0000

    refactor: products/models.py

--- products/models.py
+++ products/models.py
@@ -1,29 +1,74 @@
 from django.db import models
 
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.db import models
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 class Category(models.Model):
+    """
+    A class for the Category model
+    """
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        """
+        A class for the Meta of Categories
+        """
+
+        verbose_name_plural = "Categories"
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
+        """
+        Returns the category name string
+        Args:
+            self (object): self.
+        Returns:
+            The category name string
+        """
         return self.name
 
     def get_friendly_name(self):
+        """
+        Returns the category friendly name string
+        Args:
+            self (object): self.
+        Returns:
+            The category friendly name string
+        """
         return self.friendly_name
 
 
 class Product(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    """
+    A class for the Product Model
+    """
+
+    category = models.ForeignKey(
+        "Category", null=True, blank=True, on_delete=models.SET_NULL
+    )
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    spicerating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    spicerating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+    )
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
+        """
+        Returns the product name
+        Args:
+            self (object): self.
+        Returns:
+            The product name string
+        """
         return self.name
