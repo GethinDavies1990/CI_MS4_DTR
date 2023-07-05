5e122e9322a63fbafbc11f3c28d9ecc67d3752ba
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-05 19:50:47 +0000

    refactor: products/forms.py

--- products/apps.py
+++ products/apps.py
@@ -1,6 +1,16 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.apps import AppConfig
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class ProductsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
+    """
+    A class for configuring the products app
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "products"
