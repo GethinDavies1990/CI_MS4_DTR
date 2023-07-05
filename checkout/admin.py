c991e27a2eb18c6267272cc34fb0c6b40eda89dd
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-05 18:59:26 +0000

    refactor: checkout/admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,33 +1,78 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
-from.models import Order, OrderLineItem
+
+# Internal:
+from .models import Order, OrderLineItem
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 class OrderLineItemAdminInline(admin.TabularInline):
+    """
+    Admin class for the OrderLineItem model
+    """
+
     model = OrderLineItem
-    readonly_fields = ('lineitem_total',)
-    
-    fields = ('product', 'quantity', 'lineitem_total',)
-    
+    readonly_fields = ("lineitem_total",)
+
+    fields = (
+        "product",
+        "quantity",
+        "lineitem_total",
+    )
+
     extra = 0
 
 
 class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin class for the Order model
+    """
+
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date',
-                        'delivery_cost', 'order_total',
-                        'grand_total', 'original_bag', 'stripe_pid')
-    
-    fields = ('order_number', 'user_profile', 'date', 'full_name', 
-                'email', 'phone_number', 'country', 
-                'postcode', 'town_or_city', 'street_address1',
-                'street_address2', 'county', 'delivery_cost',
-                'order_total', 'grand_total',)
+    readonly_fields = (
+        "order_number",
+        "date",
+        "delivery_cost",
+        "order_total",
+        "grand_total",
+        "original_bag",
+        "stripe_pid",
+    )
 
-    list_display = ('order_number', 'date', 'full_name',
-                    'order_total', 'delivery_cost',
-                    'grand_total', 'original_bag',
-                    'stripe_pid')
+    fields = (
+        "order_number",
+        "user_profile",
+        "date",
+        "full_name",
+        "email",
+        "phone_number",
+        "country",
+        "postcode",
+        "town_or_city",
+        "street_address1",
+        "street_address2",
+        "county",
+        "delivery_cost",
+        "order_total",
+        "grand_total",
+    )
 
-    ordering = ('-date',)
+    list_display = (
+        "order_number",
+        "date",
+        "full_name",
+        "order_total",
+        "delivery_cost",
+        "grand_total",
+        "original_bag",
+        "stripe_pid",
+    )
+
+    ordering = ("-date",)
+
 
 admin.site.register(Order, OrderAdmin)
