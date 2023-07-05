4ccedc872a34e3b05133f8da4d217f8b9531e109
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-05 19:03:47 +0000

    refactor: checkout/forms.py

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,9 +1,19 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.apps import AppConfig
+
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class CheckoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'checkout'
+    """
+    A class for the checkout configuration
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "checkout"
 
     def ready(self):
         import checkout.signals
