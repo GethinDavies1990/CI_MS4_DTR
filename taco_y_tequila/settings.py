fabae979f79332a638c3ad74959cd7ec18107959
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-06 19:11:55 +0000

    stripe error fix: folder structure incorrect

--- taco_y_tequila/settings.py
+++ taco_y_tequila/settings.py
@@ -198,7 +198,7 @@ if "USE_AWS" in os.environ:
         "CacheControl": "max-age=94608000",
     }
 
-    # Bucket config
+    # Bucket configs
     AWS_STORAGE_BUCKET_NAME = "taco-y-tequila"
     AWS_S3_REGION_NAME = "eu-north-1"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
