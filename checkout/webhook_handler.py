a26710bd2110a25490fb89b1f9abb4ab35f615b1
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-06 18:49:39 +0000

    fix: updated code for stripe webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -22,7 +22,7 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         """
-        Initilisation of handler
+        Initialization of handler
         Args:
             request (object): Request object
         Returns:
@@ -77,6 +77,11 @@ class StripeWH_Handler:
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
+
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(
+        intent.latest_charge
+        )
 
         billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
