99eb6355f2092e5b326d2e36f43b4b0685086afb
GethinDavies1990 <gethindavies1990@gmail.com>
2023-07-06 19:41:05 +0000

    refactor: moved toast to js file

---
+++ static/js/send_email.js
@@ -0,0 +1,68 @@
+
+//  global variables added to top of js file, js hint kept showing these as unused because they are in an external file
+/* global swal */
+/* global emailjs */
+
+// function to send email in contact form.
+(function () {
+  emailjs.init("ga5PLTovBmNLpTpJO");
+})();
+
+// Subsribe email function, if a user inputs their email address into the subscribe-box they will get added to our contact list
+function validate() {
+    let subscribeEmail = document.getElementById("subscribe-email");
+    let subscribeSubmit = document.getElementById("subscribe-submit");
+    subscribeSubmit.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (subscribeEmail.value == "") {
+        subEmptyerror();
+      } else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(subscribeEmail.value)) {
+        subInvalid();
+      } else {
+        sendmail(subscribeEmail.value);
+        subSuccess();
+      }
+    });
+  }
+  validate();
+
+  // function used to send the email using emailjs
+  /**
+   *
+   * @param email [subscribe email]
+   */
+  function sendmail(email) {
+    emailjs.send("service_qknmyza", "template_yfl3t0n", {
+      "subscribe-email": email,
+    });
+  }
+
+/**
+ * Sweet Alert library used to display feedback on form submission
+ * Function called if fields are empty and a submit is attempted
+ */
+  function subEmptyerror() {
+    swal({
+      icon: "error",
+      title: "Oops...",
+      text: "Fields cannot be empty!",
+    });
+  }
+
+//   function called if the submit button is successful
+  function subSuccess() {
+    swal({
+      icon: "success",
+      title: "Success...",
+      text: "You have subscribed!",
+    });
+  }
+
+   //   function called if an invalid email address is used
+   function subInvalid() {
+    swal({
+      icon: "error",
+      title: "Oops...",
+      text: "Invalid Email Address",
+    });
+  }
--- static/js/subscribe.js
+++
@@ -1,6 +0,0 @@
-
-(function () {
-    emailjs.init("ga5PLTovBmNLpTpJO");
-  })();
-
-  emailjs.send(serviceID, templateID, templateParams, publicKey);
